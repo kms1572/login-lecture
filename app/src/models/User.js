073bb3a7a8e27079b94ad5c7f3d1edeafaca18f3
kms1572 <rlaalstn752@naver.com>
2022-04-07 16:55:25 +0900

    fs모듈을 통한 로그인 기능 구현

--- app/src/models/User.js
+++ app/src/models/User.js
@@ -8,9 +8,15 @@ class User {
         this.body = body;
     }
 
-    login() {
+    async login() {
         const client = this.body
-        const { id, psword } = UserStorage.getUsersInfo(client.id)
+        const { id, psword } = await UserStorage.getUsersInfo(client.id);
+        // # Promise를 반환하기 때문에 .then()으로 접근항 데이터를 가져올 수 있습니다.
+        // # await을 사용해준 이유는 "가독성" 입니다!
+        // # fs(파일시스템)에서도 await으로 가져올 수 있습니다.
+        // # await은 Promise를 반환하는 애한테 주는 옵션
+        // # await은 async 함수 안에서만 사용가능
+
         if (id) {
 
             if (id === client.id && psword === client.psword) {
--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -1,16 +1,21 @@
 "use strict"
 
+const fs = require("fs").promises;
 
 class UserStorage {
-    static #users = {
-        id: ["kms", "yjyj"],
-        psword: ["123", "122"],
-        name: ['민수', '윤정']
+    static #getUserInfo(data, id) {
+        const users = JSON.parse(data);
+        const idx = users.id.indexOf(id);
+        const userKeys = Object.keys(users); // => [id, psword, name]
+        const userInfo = userKeys.reduce((newUsers, info) => {
+            newUsers[info] = users[info][idx];
+            return newUsers;
+        }, {});
+        return userInfo;
     }
 
-
     static getUsers(...fields) {
-        const users = this.#users;
+        // const users = this.#users;
         const newUsers = fields.reduce((newUsers, field) => {
             if (users.hasOwnProperty(field)) {
                 newUsers[field] = users[field]
@@ -22,19 +27,18 @@ class UserStorage {
     }
 
     static getUsersInfo(id) {
-        const users = this.#users;
-        const idx = users.id.indexOf(id);
-        const userKeys = Object.keys(users); // => [id, psword, name]
-        const userInfo = userKeys.reduce((newUsers, info) => {
-            newUsers[info] = users[info][idx];
-
-            return newUsers;
-        }, {})
-        return userInfo;
+        return fs
+            .readFile("./src/databases/users.json")
+            .then((data) => {
+                return this.#getUserInfo(data, id);
+            })
+            .catch(console.error);
     }
 
+
+
     static save(userInfo) {
-        const users = this.#users;
+        // const users = this.#users;
         users.id.push(userInfo.id);
         users.name.push(userInfo.name);
         users.psword.push(userInfo.psword);
